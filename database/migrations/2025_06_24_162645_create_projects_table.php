08840ec13479e7f70b633cc6dedaac0979f3a8df
alfMG-bit <alfonso.morales2804@gmail.com>
2025-06-25 18:20:10 -0600

    Creation of tables: users, prjects, inputs, outputs and project_users

---
+++ database/migrations/2025_06_24_162645_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')
+                    ->constrained()
+                    ->references('id')
+                    ->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
