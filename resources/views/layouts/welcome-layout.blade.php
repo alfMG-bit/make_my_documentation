0fcd1d5487d7af53312494cca5ee67bb85c6290f
dunkelheitEXE <alangmoreno02@gmail.com>
2025-08-09 23:40:58 -0600

    submited to internet

--- resources/views/layouts/welcome-layout.blade.php
+++ resources/views/layouts/welcome-layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Make My Documentation</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
-    @vite(["resources/css/app.css","resources/js/app.js"])
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <style>
         body {
             font-family: 'Poppins', sans-serif;
